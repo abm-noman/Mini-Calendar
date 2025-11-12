96cd1131560496df6228f2a6fdfa766dc724ba88
abm-noman <noman15-2839@diu.edu.bd>
2025-11-12 19:39:05 +0600

    JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+const monthNames = document.getElementById("month");
+const dayNames = document.getElementById("day");
+const dateNumber = document.getElementById("date-number");
+const yearNumber = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+
+monthNames.innerHTML = date.toLocaleDateString("default", { month: "long" });
+
+dayNames.innerHTML = date.toLocaleDateString("default", { weekday: "long" });
+
+dateNumber.innerHTML = date.getDate();
+
+yearNumber.innerHTML = date.getFullYear();
+
+
+
